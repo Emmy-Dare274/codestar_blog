b52ec59e83d2bcaf12e49269431841af2615f559
Emmanuel Oluwadare <emmy4toz2010@gmail.com>
2026-02-18 02:42:24 +0100

    Fix missing setuptools dependency

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!+fjct(qhv)&(e4j_^#)frs32u!ez3i4j=ia2v=4zfa1bx*fdn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
