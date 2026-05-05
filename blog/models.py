2d4609a32b6300f4c27ac1ca249ef12368d3ffb9
Emmanuel Oluwadare <emmy4toz2010@gmail.com>
2026-05-05 09:08:43 +0100

    Comments added to blog Posts

--- blog/models.py
+++ blog/models.py
@@ -20,6 +20,31 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     
 
+    class Meta:
+        ordering = ["created_on"]
+        
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="commenter"
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    
+    
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
     
