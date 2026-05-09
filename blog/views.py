b1ae375128679b52813e110fa27d927dcf5be18a
Emmanuel Oluwadare <emmy4toz2010@gmail.com>
2026-05-09 18:12:40 +0100

    Top level templates added to static file and updated for frontend views

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,9 @@ from .models import Post
 class PostList(generic.ListView):
     #model = Post
     queryset = Post.objects.all().order_by('-created_on')
-    template_name = 'post_list.html'
+    #template_name = 'post_list.html'
+    template_name = "blog/index.html"
+    paginate_by = 6
     
 
 
--- codestar/settings.py
+++ codestar/settings.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 
 if "DATABASE_URL" in os.environ:
@@ -77,7 +79,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -151,6 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
