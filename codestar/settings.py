90c8252cdfa132ee863ebe911b06fe94498b7911
Emmanuel Oluwadare <emmy4toz2010@gmail.com>
2026-05-14 19:12:09 +0100

    About page created for our Django app blog and updated with an image

--- codestar/settings.py
+++ codestar/settings.py
@@ -46,7 +46,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 SECRET_KEY = 'django-insecure-!+fjct(qhv)&(e4j_^#)frs32u!ez3i4j=ia2v=4zfa1bx*fdn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'about',
     'django_summernote',
 ]
 
